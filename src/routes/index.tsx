597fb7557b0a62a845de876c39d16fe2599b2056
oxytomii <naomytocin@proton.me>
2025-02-23 19:57:01 -0500

    yes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,44 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <div class="flex flex-col h-screen text-black bg-white">
-      <br/>
-      <h1 class='eb-garamond-bold text-center text-2xl'>china cookbook</h1>
-      <div class="eb-garamond-norm flex h-screen text-left items-center">
-        <div class="m-20 relative">
-          fart
-          <br/>
-          
-          <img src="https://christieathome.com/wp-content/uploads/2021/07/Egg-Fried-Rice-5-500x500.jpg" class="rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-110 hover:translate-y-2" width="500" height="500"/>
+    <div
+      class="flex flex-col h-screen text-white"
+      style={{
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1482859602406-7659b00979fb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1 class="eb-garamond-bold text-center text-4xl">china cookbook</h1>
+      <div class="eb-garamond-norm flex h-screen items-center">
+        <div class="m-20 flex">
+          <div class="relative rounded-lg hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110 hover:translate-y-2 opacity-20 hover:opacity-100">
+            <img
+              src="https://christieathome.com/wp-content/uploads/2021/07/Egg-Fried-Rice-5-500x500.jpg"
+              class="rounded-4xl hover:opacity-100"
+              width="500"
+              height="500"
+            />
+            <div
+              class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300 ease-in-out transform hover:scale-110 hover:translate-y-2"
+              //style={{ pointerEvents: "none" }}
+            >
+              <p class="zhi-mang-xing-regular text-2xl scale-500 text-gray-900">
+                米
+              </p>
+            </div>
+          </div>
+          <div class="ml-10 flex-l">
+            <p class="dookie-text eb-garamond-norm text-2xl md:first-letter:text-5xl">
+              dookie dookiedookie dookie edookiedookie dookiedookie dookie
+            </p>
+          </div>
         </div>
       </div>
     </div>
